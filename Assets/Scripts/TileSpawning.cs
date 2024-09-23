f29b36c55daed6840d67772354d5f10024fa9afc
PBWiseman <wisepb1@student.op.ac.nz>
2024-09-24 08:50:27 +1200

    Added tilesheet

--- Assets/Scripts/TileSpawning.cs
+++ Assets/Scripts/TileSpawning.cs
@@ -13,6 +13,16 @@ public class TileSpawning : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+            public GameObject tilePrefab; // Assign a prefab with a SpriteRenderer component
+    public Sprite[] tileSprites; // Assign the sliced sprites in the Inspector
+
+    void Start()
+    {
+        for (int i = 0; i < tileSprites.Length; i++)
+        {
+            GameObject tile = Instantiate(tilePrefab, new Vector3(i, 0, 0), Quaternion.identity);
+            tile.GetComponent<SpriteRenderer>().sprite = tileSprites[i];
+        }
+    }
     }
 }
