29c85438508e7b27ec591fea74fe9dcd5ce2110b
Palin Wiseman <wisepb1@student.op.ac.nz>
2025-07-01 00:12:33 +1200

    CHANGE: Tile spawning now works with free movement

--- Assets/Scripts/Debris/DebrisSpawning.cs
+++ Assets/Scripts/Debris/DebrisSpawning.cs
@@ -1,3 +1,5 @@
+//Tile spawning changes broke this script. Fix later
+//TODO: Fix this to use the new TileSpawning system
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -34,56 +36,56 @@ public class DebrisSpawning : MonoBehaviour
     void Update()
     {
         //If there are less than 10 debris objects and there isn't one in this row, have a 1 in 1000 chance of spawning one
-        if (debrisObjects.Count < 10 && !spawnedRows.Contains(TileSpawning.Instance.topRightCell.y + 1) && Random.Range(0, 100) == 0)
-        {
-            //Add each horizontal cell at the top of the baseTilemap, no matter the size of the grid, to the list of spawn locations
-            List<Vector3Int> availableLocations = new List<Vector3Int>();
-            for (int x = TileSpawning.Instance.bottomLeftCell.x + 1; x < TileSpawning.Instance.topRightCell.x - 1; x++)
-            {
-                //If there isn't something in the cell, add it to the list of available locations
-                if (TileSpawning.Instance.baseTilemap.GetTile(new Vector3Int(x, TileSpawning.Instance.topRightCell.y, 0)) == null)
-                {
-                    availableLocations.Add(new Vector3Int(x, TileSpawning.Instance.topRightCell.y + 1, 0));
-                }
-            }
-            
-            if (availableLocations.Count > 0)
-            {
-                spawnDebris(availableLocations[Random.Range(0, availableLocations.Count)]);
-            }
-        }
-        //If the oldest debris object is below the bottom of the screen, remove it
-        if (debrisObjects.Count() != 0 && debrisObjects[0] != null && debrisObjects[0].transform.localPosition.y < TileSpawning.Instance.bottomLeftCell.y)
-        {
-            GameObject debris = debrisObjects[0];
-            debrisObjects.Remove(debris);
-            spawnedRows.Remove(spawnedRows.Min());
-            Destroy(debris);
-        }
+        // if (debrisObjects.Count < 10 && !spawnedRows.Contains(TileSpawning.Instance.topRightCell.y + 1) && Random.Range(0, 100) == 0)
+        // {
+        //     //Add each horizontal cell at the top of the baseTilemap, no matter the size of the grid, to the list of spawn locations
+        //     List<Vector3Int> availableLocations = new List<Vector3Int>();
+        //     for (int x = TileSpawning.Instance.bottomLeftCell.x + 1; x < TileSpawning.Instance.topRightCell.x - 1; x++)
+        //     {
+        //         //If there isn't something in the cell, add it to the list of available locations
+        //         if (TileSpawning.Instance.baseTilemap.GetTile(new Vector3Int(x, TileSpawning.Instance.topRightCell.y, 0)) == null)
+        //         {
+        //             availableLocations.Add(new Vector3Int(x, TileSpawning.Instance.topRightCell.y + 1, 0));
+        //         }
+        //     }
+
+        //     if (availableLocations.Count > 0)
+        //     {
+        //         spawnDebris(availableLocations[Random.Range(0, availableLocations.Count)]);
+        //     }
+        // }
+        // //If the oldest debris object is below the bottom of the screen, remove it
+        // if (debrisObjects.Count() != 0 && debrisObjects[0] != null && debrisObjects[0].transform.localPosition.y < TileSpawning.Instance.bottomLeftCell.y)
+        // {
+        //     GameObject debris = debrisObjects[0];
+        //     debrisObjects.Remove(debris);
+        //     spawnedRows.Remove(spawnedRows.Min());
+        //     Destroy(debris);
+        // }
     }
 
     private void spawnDebris(Vector3Int cell)
     {
-        //Select a random debris prefab based on rarity. If object A has a 15 ChanceToSpawn and object B has a 5 ChanceToSpawn, object A will be selected 3/4 of the time
-        int totalChance = 0;
-        foreach (DebrisRarity debris in debrisPrefabs)
-        {
-            totalChance += debris.ChanceToSpawn;
-        }
-        int randomChance = Random.Range(0, totalChance);
-        GameObject debrisPrefab = null;
-        foreach (DebrisRarity debris in debrisPrefabs)
-        {
-            randomChance -= debris.ChanceToSpawn;
-            if (randomChance <= 0)
-            {
-                debrisPrefab = debris.debrisPrefab;
-                break;
-            }
-        }
-        GameObject spawnedDebris = Instantiate(debrisPrefab, TileSpawning.Instance.baseTilemap.GetCellCenterWorld(cell), Quaternion.identity);
-        spawnedDebris.transform.SetParent(TileSpawning.Instance.grid.transform);
-        debrisObjects.Add(spawnedDebris);
-        spawnedRows.Add(cell.y);
+        // //Select a random debris prefab based on rarity. If object A has a 15 ChanceToSpawn and object B has a 5 ChanceToSpawn, object A will be selected 3/4 of the time
+        // int totalChance = 0;
+        // foreach (DebrisRarity debris in debrisPrefabs)
+        // {
+        //     totalChance += debris.ChanceToSpawn;
+        // }
+        // int randomChance = Random.Range(0, totalChance);
+        // GameObject debrisPrefab = null;
+        // foreach (DebrisRarity debris in debrisPrefabs)
+        // {
+        //     randomChance -= debris.ChanceToSpawn;
+        //     if (randomChance <= 0)
+        //     {
+        //         debrisPrefab = debris.debrisPrefab;
+        //         break;
+        //     }
+        // }
+        // GameObject spawnedDebris = Instantiate(debrisPrefab, TileSpawning.Instance.baseTilemap.GetCellCenterWorld(cell), Quaternion.identity);
+        // spawnedDebris.transform.SetParent(TileSpawning.Instance.grid.transform);
+        // debrisObjects.Add(spawnedDebris);
+        // spawnedRows.Add(cell.y);
     }
 }
--- Assets/Scripts/TileSpawning.cs
+++ Assets/Scripts/TileSpawning.cs
@@ -1,4 +1,7 @@
-using System.Collections;
+/// <summary>
+/// Reworking the system to use a preset map and free movement
+/// Just having it spawn water tiles in all places in view for the moment
+/// </summary>
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -6,14 +9,14 @@ using System;
 using Random = UnityEngine.Random;
 
 //A row of tiles
-[System.Serializable]
+[Serializable]
 public class TileRow
 {
     public TileBase[] tiles;
 }
 
 //A set of rows. This is used to create a set 3d grid of tiles for the port or other fixed structures
-[System.Serializable]
+[Serializable]
 public class TileLayer
 {
     public TileRow[] rows;
@@ -23,27 +26,12 @@ public class TileSpawning : MonoBehaviour
 {
     public Grid grid;
     public Tilemap waterTilemap;
-    public Tilemap baseTilemap;
-    public Tilemap decorationTilemap;
-    //Water tile
     public TileBase waterTile;
-    public TileBase landTile;
-    public TileBase coastlineTile;
-    public TileBase[] decorationTiles;
-
-    //3d grid of tiles for the port
-    public TileLayer portTiles;
-
-    public Vector3Int bottomLeftCell;
-    public Vector3Int topRightCell;
     public static TileSpawning Instance;
-
-    public List<Vector3Int> coastLines;
     private Camera mainCamera;
-
-    private float nextPortTimer = 0;
-    private bool portSpawning = false;
-    private int portIndex = 0;
+    private HashSet<Vector3Int> currentlyVisibleTiles = new HashSet<Vector3Int>();
+    private Vector3 lastCameraPosition;
+    private Quaternion lastCameraRotation;
 
     void Awake()
     {
@@ -57,6 +45,7 @@ public class TileSpawning : MonoBehaviour
         }
         int refreshRate;
         //Cap this at the lesser of the screen's refresh rate and 60fps
+        //TODO: Fix this to be the new way of doing it? Apparently this is deprecated
         if (Screen.currentResolution.refreshRate < 0)
         {
             refreshRate = 60;
@@ -67,172 +56,96 @@ public class TileSpawning : MonoBehaviour
         }
         Application.targetFrameRate = refreshRate;
         mainCamera = Camera.main;
-        getCorners();
-        coastLines = new List<Vector3Int> { topRightCell, topRightCell - new Vector3Int(1, 0, 0) }; //Hardcoding the rightmost two columns of the screen to be coast
     }
 
     void Start()
     {
-        fillStartWater();
-        spawnStartCoast(coastLines);
-        spawnStartDecorations(coastLines);
+
     }
 
     private void Update()
     {
-        //Move the grid down 2 unit per second
-        //grid.transform.position -= new Vector3(0, 2 * Time.deltaTime, 0);
-        //Every x amount of time start to make a port
-        //if (Time.time > nextPortTimer)
-        //{
-            //Spawn port
-            //portSpawning = true;
-            //nextPortTimer = Time.time + Random.Range(30, 60);
-        //}
-        spawnTopRow(coastLines);
-        despawnBottomRow();
+        if (mainCamera == null) return;
+
+        // Only update if the camera has moved or rotated
+        if (mainCamera.transform.position != lastCameraPosition || mainCamera.transform.rotation != lastCameraRotation)
+        {
+            spawnTiles();
+            lastCameraPosition = mainCamera.transform.position;
+            lastCameraRotation = mainCamera.transform.rotation;
+        }
+    }
+
+    private void spawnTiles()
+    {
+        HashSet<Vector3Int> newVisibleTiles = GetVisibleTiles();
+
+        // Tiles to spawn
+        foreach (var cell in newVisibleTiles)
+        {
+            if (!currentlyVisibleTiles.Contains(cell))
+            {
+                waterTilemap.SetTile(cell, waterTile);
+                //waterTilemap.SetTransformMatrix(cell, Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0, 0, Random.Range(0, 4) * 90), Vector3.one));
+            }
+        }
+
+        // Tiles to despawn
+        foreach (var cell in currentlyVisibleTiles)
+        {
+            if (!newVisibleTiles.Contains(cell))
+            {
+                waterTilemap.SetTile(cell, null);
+            }
+        }
+
+        currentlyVisibleTiles = newVisibleTiles;
     }
 
     //Get the corners of the camera's view in world coordinates
-    private void getCorners()
+    private HashSet<Vector3Int> GetVisibleTiles()
     {
-        Camera mainCamera = Camera.main;
-        if (mainCamera == null)
+        if (mainCamera == null || waterTilemap == null)
         {
-            return;
+            return new HashSet<Vector3Int>();
         }
 
-        //Get the corners of the camera's view in world coordinates
-        Vector3 bottomLeft = mainCamera.ScreenToWorldPoint(new Vector3(0, 0, mainCamera.nearClipPlane));
-        Vector3 topRight = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.nearClipPlane));
+        // Get the four corners of the camera's view in world space
+        Vector3[] worldCorners = new Vector3[4];
+        worldCorners[0] = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, mainCamera.nearClipPlane)); // Bottom Left
+        worldCorners[1] = mainCamera.ViewportToWorldPoint(new Vector3(1, 0, mainCamera.nearClipPlane)); // Bottom Right
+        worldCorners[2] = mainCamera.ViewportToWorldPoint(new Vector3(1, 1, mainCamera.nearClipPlane)); // Top Right
+        worldCorners[3] = mainCamera.ViewportToWorldPoint(new Vector3(0, 1, mainCamera.nearClipPlane)); // Top Left
 
-        //Convert world coordinates to cell positions in the Tilemap
-        bottomLeftCell = waterTilemap.WorldToCell(bottomLeft);
-        topRightCell = waterTilemap.WorldToCell(topRight);
-    }
-
-    //Fill the Tilemap with water tiles within the calculated bounds
-    private void fillStartWater()
-    {
-        //Fill the Tilemap with water tiles within the calculated bounds
-        for (int x = bottomLeftCell.x; x <= topRightCell.x; x++)
+        // Convert to cell positions
+        Vector3Int[] cellCorners = new Vector3Int[4];
+        for (int i = 0; i < 4; i++)
         {
-            for (int y = bottomLeftCell.y; y <= topRightCell.y; y++)
-            {
-                waterTilemap.SetTile(new Vector3Int(x, y, 0), waterTile);
-                //Rotate the water tiles randomly
-                waterTilemap.SetTransformMatrix(new Vector3Int(x, y, 0), Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0, 0, Random.Range(0, 4) * 90), Vector3.one));
-            }
+            cellCorners[i] = waterTilemap.WorldToCell(worldCorners[i]);
         }
-    }
 
-    //Spawn layers of coast on the inputted x lines of the of the screen
-    private void spawnStartCoast(List<Vector3Int> xLines)
-    {
-        foreach (Vector3Int cell in xLines)
-        {
-            for (int y = bottomLeftCell.y; y <= topRightCell.y; y++)
-            {
-                baseTilemap.SetTile(new Vector3Int(cell.x, y, 0), landTile);
-            }
-            baseTilemap.SetTile(cell, landTile);
-        }
-    }
+        // Find bounds (min/max x and y)
+        int minX = Mathf.Min(cellCorners[0].x, cellCorners[1].x, cellCorners[2].x, cellCorners[3].x);
+        int maxX = Mathf.Max(cellCorners[0].x, cellCorners[1].x, cellCorners[2].x, cellCorners[3].x);
+        int minY = Mathf.Min(cellCorners[0].y, cellCorners[1].y, cellCorners[2].y, cellCorners[3].y);
+        int maxY = Mathf.Max(cellCorners[0].y, cellCorners[1].y, cellCorners[2].y, cellCorners[3].y);
 
-    //Spawn layers of decorations on the inputted x lines of the of the screen
-    private void spawnStartDecorations(List<Vector3Int> xLines)
-    {
-        foreach (Vector3Int cell in xLines)
+        // Collect all cells within the rectangle that are actually inside the camera frustum
+        HashSet<Vector3Int> visibleCells = new HashSet<Vector3Int>();
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(mainCamera);
+
+        for (int x = minX; x <= maxX; x++)
         {
-            for (int y = bottomLeftCell.y; y <= topRightCell.y; y++)
+            for (int y = minY; y <= maxY; y++)
             {
-                if (Random.Range(0, 100) < 30)
+                Vector3 cellCenter = waterTilemap.GetCellCenterWorld(new Vector3Int(x, y, 0));
+                if (GeometryUtility.TestPlanesAABB(planes, new Bounds(cellCenter, Vector3.one)))
                 {
-                    decorationTilemap.SetTile(new Vector3Int(cell.x, y, 0), decorationTiles[Random.Range(0, decorationTiles.Length)]);
-                    decorationTilemap.SetTransformMatrix(new Vector3Int(cell.x, y, 0), Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0, 0, Random.Range(0, 4) * 90), Vector3.one));
+                    visibleCells.Add(new Vector3Int(x, y, 0));
                 }
             }
         }
+        return visibleCells;
     }
 
-    //Despawn the bottom row of tiles if they are out of view
-    private void despawnBottomRow()
-    {
-        if (mainCamera == null)
-        {
-            return;
-        }
-
-        // Get the bottom edge of the camera's view in world coordinates
-        Vector3 bottomEdge = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width / 2, 0, mainCamera.nearClipPlane));
-        Vector3Int bottomEdgeCell = waterTilemap.WorldToCell(bottomEdge);
-
-        // Check if the bottom row of tiles is out of view and remove them
-        if (bottomEdgeCell.y > bottomLeftCell.y)
-        {
-            for (int x = bottomLeftCell.x; x <= topRightCell.x; x++)
-            {
-                waterTilemap.SetTile(new Vector3Int(x, bottomLeftCell.y, 0), null);
-                baseTilemap.SetTile(new Vector3Int(x, bottomLeftCell.y, 0), null);
-                decorationTilemap.SetTile(new Vector3Int(x, bottomLeftCell.y, 0), null);
-            }
-            bottomLeftCell.y++;
-        }
-    }
-
-    //Spawn the top row of tiles if they are going to be in view
-    //Also spawn coast and decorations on the inputted x lines
-    private void spawnTopRow(List<Vector3Int> xLines)
-    {
-        if (mainCamera == null)
-        {
-            return;
-        }
-
-        // Get the top edge of the camera's view in world coordinates
-        Vector3 topEdge = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height, mainCamera.nearClipPlane));
-        Vector3Int topEdgeCell = waterTilemap.WorldToCell(topEdge);
-
-        // Check if the top row of tiles is going to be in view and spawn them
-        if (topEdgeCell.y + 1 > topRightCell.y)
-        {
-            topRightCell.y++;
-            for (int x = bottomLeftCell.x; x <= topRightCell.x; x++)
-            {
-                waterTilemap.SetTile(new Vector3Int(x, topRightCell.y, 0), waterTile);
-                waterTilemap.SetTransformMatrix(new Vector3Int(x, topRightCell.y, 0), Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0, 0, Random.Range(0, 4) * 90), Vector3.one));
-            }
-            if (portSpawning)
-            {
-                int column = topRightCell.x;
-                //Spawn the port tiles of the index starting from the right of the screen using the portIndex to tell what row
-                for (int i = portTiles.rows[portIndex].tiles.Length - 1; i >= 0; i--) //Because I put the port tiles in the array backwards and this is easier than changing them all
-                {
-                    decorationTilemap.SetTile(new Vector3Int(column, topRightCell.y, 0), portTiles.rows[portIndex].tiles[i]);
-                    if (portTiles.rows[portIndex].tiles[i] != null || portTiles.rows[portIndex].tiles[0] != null)
-                    {
-                        baseTilemap.SetTile(new Vector3Int(column, topRightCell.y, 0), landTile);
-                    }
-                    column--;
-                }
-                portIndex++;
-                if (portIndex >= portTiles.rows.Length)
-                {
-                    portSpawning = false;
-                    portIndex = 0;
-                }
-            }
-            else
-            {
-                foreach (Vector3Int cell in xLines)
-                {
-                    baseTilemap.SetTile(new Vector3Int(cell.x, topRightCell.y, 0), landTile);
-                    if (Random.Range(0, 100) < 30)
-                    {
-                        decorationTilemap.SetTile(new Vector3Int(cell.x, topRightCell.y, 0), decorationTiles[Random.Range(0, decorationTiles.Length)]);
-                    }
-                }
-            }
-        }
-    }
 }
